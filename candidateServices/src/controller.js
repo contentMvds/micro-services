b551cf60f52db60aa67b9a81d96a69cec2a449d8
Marcilio Victor <cilho_3@hotmail.com>
2020-12-13 22:44:17 -0300

    include functions models, and inicial logic for controller

--- candidateServices/src/controller.js
+++ candidateServices/src/controller.js
@@ -1,40 +1,56 @@
+import db from './db';
+
 const getAll = () => {
   let result;
   try {
+    result = await db.getAll();
   } catch (error) {
     result = error;
   }
   return result;
 };
-const get = () => {
+const get = async (param) => {
   let result;
   try {
+    result = await db.get(id);
   } catch (error) {
     result = error;
   }
   return result;
 };
-const post = () => {
+const post = (param) => {
   let result;
   try {
+    result = await db.post(param);
+
   } catch (error) {
     result = error;
   }
   return result;
 };
-const put = () => {
+const put = (param) => {
   let result;
   try {
+    result = await db.put(id, param);
   } catch (error) {
     result = error;
   }
   return result;
 };
-const del = () => {
+const del = (param) => {
   let result;
   try {
+    result = await db.del(id);
   } catch (error) {
     result = error;
   }
   return result;
 };
+
+export default {
+  get,
+  getAll,
+  post,
+  put,
+  del
+}
--- candidateServices/src/db.js
+++ candidateServices/src/db.js
@@ -7,11 +7,39 @@ const schema = mongoose.model(
     email: { type: String, default: '', required: true },
     skill: { type: [Object], default: {}, required: true },
     needs: { type: [Object], default: {} },
+    data: { type: Date, default: Date.now() },
   })
 );
 
-const cadidateDB = () => {};
-const candidatesDB = () => {};
-const createCandidatesDB = () => {};
-const updateCondidatesDB = () => {};
-const deleteCondidatesDB = () => {};
+const candidatesDB = async () => {
+  const result = await schema.find().exec();
+  return result;
+};
+const candidateDB = async (_id) => {
+  if (!_id) throw new Error('Identifier not found get');
+  const result = await schema.findById({ _id }).exec();
+  return result;
+};
+const createCandidatesDB = async (obj) => {
+  if (!db) throw new Error('register not found create');
+  result = await schema.create(obj).exec();
+  return result;
+};
+const updateCondidatesDB = async (_id, obj) => {
+  if (!_id || !obj) throw new Error('Parameters not found in upadate');
+  result = await schema.updateOne({ _id }, obj).exec();
+  return result;
+};
+const deleteCondidatesDB = async (_id) => {
+  if (!_id) throw new Error('Identifier not found in delete');
+  result = await schema.deleteOne({ _id }).exec();
+  return result;
+};
+
+export default {
+  get: candidateDB,
+  getAll: candidatesDB,
+  post: createCandidatesDB,
+  pull: updateCondidatesDB,
+  del: deleteCondidatesDB,
+};
